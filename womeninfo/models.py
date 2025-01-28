28b7c0baad8c39e7a112b883ea3c4c0f4bed670e
Arsen <arsenatl2003gmail.com>
2025-01-28 11:16:44 +0500

    Updated models and settings, removed unused files, added migrations

--- womeninfo/models.py
+++ womeninfo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here
 
 
@@ -8,16 +9,18 @@ class WomenModel(models.Model):
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to='images/')
-    video = models.FileField(upload_to='videos/')
-    cat=models.ForeignKey('Category',on_delete=models.PROTECT,null=True)
+    image = models.ImageField(upload_to='media/')
+    video = models.FileField(upload_to='media/')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
 
 
-class Category (models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
-    
+
     def __str__(self):
         return self.name
+
+
